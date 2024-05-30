ccbe2b0b369eee0486a126e8a71735caaba21aa0
Aben25 <abenuro@gmail.com>
2024-05-30 10:40:32 -0400

    first1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import { BarcodeScanner } from 'react-barcode-scanner'
-import "react-barcode-scanner/polyfill"
+import logo from './logo.svg';
+import './App.css';
 
-export default () => {
-  return <BarcodeScanner />
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
