115abf9c3980256d167b17418423115ca6b2b43a
Abel <se.abel.tadesse@gmail.com>
2024-05-31 21:01:35 +0300

    removing one line

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ root.render(
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
